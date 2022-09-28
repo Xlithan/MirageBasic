4d7f8bf74e247e66d85bd90c58e7c406a895c3ca
Seth <Hellator@gmail.com>
2022-09-28 01:19:10 -0500

    Remove character name when delete account.

--- Source/Server/Modules/Network/S_NetworkReceive.vb
+++ Source/Server/Modules/Network/S_NetworkReceive.vb
@@ -448,6 +448,7 @@ Module S_NetworkReceive
             End If
 
             LoadCharacter(index, slot)
+            CharactersList.Remove(Player(index).Name)
             ClearCharacter(index)
             SaveCharacter(index, slot)
             Account(index).Character(slot) = ""
